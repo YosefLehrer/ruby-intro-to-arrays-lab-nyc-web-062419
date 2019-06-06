3316a2201db427fe9f4b6c10c52f5e1e47165dcf
Yosef Lehrer <yolehrer@gmail.com>
2019-06-06 21:16:08 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -20,6 +20,10 @@ def last_element(array)
   return array[-1]
 end
 
+def first_element_with_array_methods(array)
+  return array.first()
+end
+
 def length_of_array(array)
   return array.length()
 end
