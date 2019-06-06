90109dc6d18bbd1ddba36912d4873830e897d295
Yosef Lehrer <yolehrer@gmail.com>
2019-06-06 21:09:27 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,13 @@
+def instantiate_new_array
+  my_new_array = []
+  return my_new_array
+end
+
+def array_with_two_elements
+  my_two_array = ["hello", "aloha"]
+  return my_two_array
+end
+
+def first_element(array)
+  return my_first_element = first_element(@taylor_swift)
+end
